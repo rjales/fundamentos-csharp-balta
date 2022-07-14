2ab864ae7b8fb5b32f36f2fec364faa5a1a4e3b0
Romulo Jales <romulojales@outlook.com>
2022-07-14 13:18:52 -0300

    Primeiro commit

---
+++ Calculator/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+
+            Console.WriteLine("O que deseja fazer?");
+            Console.WriteLine("");
+            Console.WriteLine("1 - Soma");
+            Console.WriteLine("2 - Subtração");
+            Console.WriteLine("3 - Multiplicação");
+            Console.WriteLine("4 - Divisão");
+
+            Console.WriteLine("-------------------");
+            Console.Write("Selecione uma opção: ");
+            short res = short.Parse(Console.ReadLine());
+
+        }
+
+        static void Soma()
+        {
+            Console.Clear();
+
+            Console.Write("Primeiro valor: ");
+            double v1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Segundo valor ");
+            double v2 = double.Parse(Console.ReadLine());
+
+            double result = v1 + v2;
+
+            Console.WriteLine($"o resultado da soma é: {result}");
+
+            Console.ReadKey();
+        }
+
+        static void Subtracao()
+        {
+            Console.Clear();
+
+            Console.Write("Primeiro valor: ");
+            double v1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Segundo valor ");
+            double v2 = double.Parse(Console.ReadLine());
+
+            double result = v1 - v2;
+
+            Console.WriteLine($"o resultado da subtração é: {result}");
+
+            Console.ReadKey();
+        }
+
+        static void Multiplicacao()
+        {
+            Console.Clear();
+
+            Console.Write("Primeiro valor: ");
+            double v1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Segundo valor ");
+            double v2 = double.Parse(Console.ReadLine());
+
+            double result = v1 * v2;
+
+            Console.WriteLine($"o resultado da multiplicação é: {result}");
+
+            Console.ReadKey();
+        }
+
+        static void Divisao()
+        {
+            Console.Clear();
+
+            Console.Write("Primeiro valor: ");
+            double v1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Segundo valor ");
+            double v2 = double.Parse(Console.ReadLine());
+
+            double result = v1 / v2;
+
+            Console.WriteLine($"o resultado da divisão é: {result}");
+
+            Console.ReadKey();
+        }
+    }
+}
